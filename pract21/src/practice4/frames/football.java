83724d1c50fa3206d5a26a8d939443af2706aafb
Mikhail <aivazyan.com@gmail.com>
2021-10-14 10:37:49 +0300

    initial commit

---
+++ pract21/src/practice4/frames/football.java
@@ -0,0 +1,52 @@
+package practice4.frames;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class football extends JFrame {
+    private static final String MILAN = "AC Milan";
+    private static final String REAL_MADRID = "Real Madrid";
+
+    JButton btnMilan = new JButton(MILAN);
+    JButton btnRealMadrid = new JButton(REAL_MADRID);
+    JLabel jlResult = new JLabel("Result: 0 X 0");
+    JLabel jlLastScorer = new JLabel("Last Scorer: N/A");
+    Label lWinner = new Label("Winner: DRAW");
+    int milanScore = 0;
+    int realMadridScore = 0;
+    String lastScorer = "N/A";
+    String winner = "N/A";
+
+    public football() {
+        super("Practice 4");
+        setSize(300, 300);
+        setLayout(new FlowLayout());
+        add(btnMilan);
+        add(btnRealMadrid);
+        add(jlResult);
+        add(jlLastScorer);
+        add(lWinner);
+
+        btnMilan.addActionListener(ae -> {
+            milanScore++;
+            jlLastScorer.setText("Last Scorer: " + MILAN);
+            onClick();
+        });
+        btnRealMadrid.addActionListener(ae -> {
+            realMadridScore++;
+            jlLastScorer.setText("Last Scorer: " + REAL_MADRID);
+            onClick();
+        });
+    }
+
+    private void onClick() {
+        jlResult.setText("Result: " + milanScore + " X " + realMadridScore);
+        if (milanScore > realMadridScore) {
+            lWinner.setText("Winner: " + MILAN);
+        } else if (realMadridScore > milanScore) {
+            lWinner.setText("Winner: " + REAL_MADRID);
+        } else {
+            lWinner.setText("Winner: DRAW");
+        }
+    }
+}
